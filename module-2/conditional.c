3e2987e7e43802e330afa95f055ac373e074120e
saifulislam106 <saifulislam106915@gmail.com>
2025-10-06 20:38:54 +0600

    commit: conditionanl c

---
+++ module-2/conditional.c
@@ -0,0 +1,36 @@
+// #include<stdio.h>
+// int main()
+// {
+//     int a;
+//     scanf("%d", &a);
+//     if(a >=10){
+//         printf("a is big");
+//     }else{
+//         printf("a is small");
+//     }
+// }
+
+#include<stdio.h>
+int main()
+{
+    int tk;
+    scanf("%d", &tk);
+    if(tk >=1000){
+        printf("I will buy a shirt");
+    }else if(tk >= 500){
+        printf("I will buy a T shirt");
+    }else{
+        printf("I Wont buy anything");
+    }
+}
+// #include<stdio.h>
+// int main()
+// {
+//     int a;
+//     scanf("%d", &a);
+//     if(a >=10){
+//         printf("a is big");
+//     }else{
+//         printf("a is small");
+//     }
+// }
