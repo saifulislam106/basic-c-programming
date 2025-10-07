7240eb6f4ff5da15968570e89805e2795aae2a5f
saifulislam106 <saifulislam106915@gmail.com>
2025-10-07 21:00:07 +0600

    pactice-1 done

---
+++ practice-1/practice.c
@@ -0,0 +1,55 @@
+// problem-1
+// #include<stdio.h>
+
+// int main()
+// {
+//     int N;
+//     scanf("%d", &N);
+//     if(N==0){
+//         printf("Zero");
+//     }else{
+//         printf("Non Zero");
+//     };
+//     return 0;
+// }
+
+// problem-2
+// #include<stdio.h>
+
+// int main()
+// {
+//     int N;
+//     scanf("%d", &N);
+//     int sum = N + 5;
+//     printf("%d", sum);
+//     return 0;
+// }
+
+// // problem-3
+// #include<stdio.h>
+
+// int main()
+// {
+//     int a;
+//     int b;
+//     scanf("%d", &a);
+//     scanf("%d", &b);
+  
+//     if(a%b == 0 || b%a == 0){
+//         printf("Yes");
+//     }else{
+//         printf("No");
+//     }
+//     return 0;
+// }
+
+// Problem-4
+#include<stdio.h>
+
+int main()
+{
+    float a;
+    scanf("%f", &a);
+    printf("%.3f", a);
+    return 0;
+}
