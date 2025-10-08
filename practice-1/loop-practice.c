e47b9e3dde98222090c54bcc5405124c1a1a888b
saifulislam106 <saifulislam106915@gmail.com>
2025-10-08 20:14:00 +0600

    for and while loop commited

---
+++ practice-1/loop-practice.c
@@ -0,0 +1,46 @@
+// #include <stdio.h>
+
+// int main()
+// {
+//     for (int i = 1; i <= 8; i++)
+//     {
+
+//         printf("200\n");
+//     };
+// }
+#include <stdio.h>
+
+int main()
+{
+    for (int i = 99; i >= 1; i=i-2)
+    {
+       if(i==51){
+        continue;;
+       }
+        printf("%d\n",i);
+    };
+}
+// #include <stdio.h>
+
+// int main()
+// {
+//     for (int i = 99; i >= 1; i=i-2)
+//     {
+//        if(i<=50){
+//         break;
+//        }
+//         printf("%d\n",i);
+//     };
+// }
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i =99;
+//     while (i>=1)
+//     {
+//         printf("%d\n",i);
+//         i= i-2;
+//     };
+// }
